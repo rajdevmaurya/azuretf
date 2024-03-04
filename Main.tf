e9e805d9104bd7a3a6e3d956ee514c7013a8e545
rajdevmaurya <rajdevmaurya@gmail.com>
2024-03-04 19:47:47 +0530

    Updated

--- Main.tf
+++ Main.tf
@@ -1,16 +1,22 @@
-# ARM provider block -rekhu
-provider "azurerm" {
-  source  = "hashicorp/azurerm"
-  version = ">=2.99.0"
-  features {}
-}
-# Terraform backend configuration block -precreated
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "rg-cloudquickpocs"
     storage_account_name = "ccpsazuretf0001"
     container_name       = "ccpterraformstatefile"
     key                  = "ccpsterraform.tfstate"
   }
+}
 
+provider "azurerm" {
+  features {}
 }
