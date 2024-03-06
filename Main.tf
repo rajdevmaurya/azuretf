3f9e828e03897dfdc9302fdd31c100e8ec423aa7
rajdevmaurya <rajdevmaurya@gmail.com>
2024-03-06 07:14:30 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -57,7 +57,7 @@ resource "azurerm_mssql_database" "db" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "tfstorage013"
+  name                     = "tfstorage01333"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
