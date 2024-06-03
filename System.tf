1a03e485f972e421afd334930d51416c65eae80f
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-03 15:35:46 +0530

    clean code

--- System.tf
+++
@@ -1,9 +0,0 @@
-module "app_service_prod" {
-	source = "./webappservice/"
-	location            = "East US"
-	environment         = "dev"
-	owner               = "RAjdev"
-	description         = "Linux webapp"
-	app_name            = "myrgroup01"
-	no_of_app_count     = 2
-}
