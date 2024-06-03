bcc726484e54aff588f2cd56d62554d600d48c49
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-03 22:48:04 +0530

    clean up

--- System.tf
+++
@@ -1,9 +0,0 @@
-module "app_service_prod" {
-	source = "./abcd_infra/"
-	location            = "East US"
-	environment         = "System"
-	owner               = "abcd"
-	os_type             = "Windows"
-	app_name            = "abcd"
-	no_of_app_count     = 2
-}
