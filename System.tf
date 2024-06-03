3f4903df91f119c19dc0b344f2136bdc8688838f
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-03 15:31:04 +0530

    Update Kubernetes manifest

---
+++ System.tf
@@ -0,0 +1,9 @@
+module "app_service_prod" {
+	source = "./webappservice/"
+	location            = "East US"
+	environment         = "dev"
+	owner               = "RAjdev"
+	description         = "Linux webapp"
+	app_name            = "myrgroup01"
+	no_of_app_count     = 2
+}
