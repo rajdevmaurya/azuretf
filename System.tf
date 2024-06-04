cf7ea5fe6920e0bd9e5ffc3fcc5b7ba533caaa07
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-04 18:19:27 +0530

    Update Kubernetes manifest

--- System.tf
+++ System.tf
@@ -1,10 +1,10 @@
 module "app_service_prod" {
-	source = "./apm1432_infra/"
+	source = "./abcd_infra/"
 	location            = "East US"
 	environment         = "System"
 	owner               = "abcd"
 	os_type             = "Windows"
-	app_name            = "apm1432"
+	app_name            = "abcd"
 	app_code            = "nia"
 	no_of_app_count     = 2
 }
