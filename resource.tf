4085924649e3437b9873ac957904f1faef0456e4
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-05 16:30:44 +0530

    clean

--- resource.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "appservice-rg" {
-  name     = "finance"
-  location = "East Us"
-  tags = {
-    description = "POCs Demo"
-    environment = "System"
-    owner       = "MyTest"  
-  }
-}
