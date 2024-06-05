e96a69c4777f3e2cd55db14e8a6b9198d37339c6
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-05 16:11:08 +0530

    Update Kubernetes manifest

---
+++ mms_System.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.99.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-cloudquickpocs"
+    storage_account_name = "ccpsazuretf0001"
+    container_name       = "ccpterraformstatefile"
+    key                  = "ccpsterraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+module "vm_service_prod" {
+	source = "./mms_infra/"
+	location            = "East US"
+	environment         = "System"
+	owner               = "abcd"
+	os_type             = "Windows"
+	app_name            = "mms1432"
+	app_code            = "mms"
+	no_of_app_count     = 2
+}
