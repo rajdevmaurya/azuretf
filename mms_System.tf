0051727b39cb05b1f38da0b49656f8be11ce5daa
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-05 10:45:27 +0530

    Update Kubernetes manifest

---
+++ mms_System.tf
@@ -0,0 +1,10 @@
+module "vm_service_prod" {
+	source = "./mms_infra/"
+	location            = "East US"
+	environment         = "System"
+	owner               = "abcd"
+	os_type             = "Linux"
+	app_name            = "apm1432"
+	app_code            = "mms"
+	no_of_app_count     = 2
+}
