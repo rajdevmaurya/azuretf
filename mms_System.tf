a3e5f53b39083ecc4da73715d5a61083896675a0
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-06 18:16:54 +0530

    Update Kubernetes manifest

---
+++ mms_System.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.99.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+module "vm_service_prod" {
+	source = "./mms_infra/"
+	location            = "East US"
+	environment         = "System"
+	owner               = "abcd"
+	os_type             = "Linux"
+	app_name            = "mms1432"
+	app_code            = "mms"
+	no_of_app_count     = 1
+}
