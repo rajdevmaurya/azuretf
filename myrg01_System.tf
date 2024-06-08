ae2d7c9d92459227c0c7b842e25e23c9df02a0b1
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-08 17:59:41 +0530

    Update Kubernetes manifest

---
+++ myrg01_System.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.6.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+module "vm_service_prod" {
+	source = "./myrg01_infra/"
+	resource_group_name     = "rg-terraform-web-sql-db"
+	resource_group_location = "East US"
+	app_service_plan_name   = "appserviceplan-web-21"
+	app_service_name        = "terraform-web-021"
+	sql_server_name         = "terraform-sqlserver-02133"
+	sql_database_name       = "ProductsDB"
+	sql_admin_login         = "user01"
+	sql_admin_password      = "@Aa123456789!"
+}
