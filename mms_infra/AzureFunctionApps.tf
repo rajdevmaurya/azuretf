f2b4e5d38d83ae354fbadeef84bfbcacfc575685
rajdevmaurya <rajdevmaurya@gmail.com>
2024-06-15 15:48:23 +0530

    Update  manifest

---
+++ mms_infra/AzureFunctionApps.tf
@@ -0,0 +1,56 @@
+resource "random_id" "identifier" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    environment = var.tag
+  }
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = var.sku_storage
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "${var.function_app_name}-asp"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  kind                = "FunctionApp"
+  reserved            = true # Required for Linux
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "main" {
+  name                       = var.function_app_name
+  location                   = var.location
+  resource_group_name        = azurerm_resource_group.main.name
+  storage_account_name       = azurerm_storage_account.main.name
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+  app_service_plan_id        = azurerm_app_service_plan.main.id
+  os_type                    = "linux"
+  version                    = var.functions_version
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "python"
+    AzureWebJobsStorage      = azurerm_storage_account.main.primary_connection_string
+  }
+
+  site_config {
+    linux_fx_version = "python|${var.python_version}"
+  }
+}
