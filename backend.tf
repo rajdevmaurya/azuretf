5d98806a2c36d5d4229b4fd2ce36e69eb0a8a3d0
rajdevmaurya <rajdevmaurya@gmail.com>
2024-08-03 19:05:09 +0530

    fds

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "backend-rg"
-    storage_account_name = "backendsa101"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
-}
