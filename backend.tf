9ab89890f0619fce09a5b5f9d22136d1258c93d5
rajdevmaurya <rajdevmaurya@gmail.com>
2024-08-03 19:30:56 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "backend-rg"
+    resource_group_name  = "backendrg"
     storage_account_name = "backendsa107"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
