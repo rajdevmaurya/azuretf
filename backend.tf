dead835135bf63cc7398ee5e14d1103258eb5995
rajdevmaurya <rajdevmaurya@gmail.com>
2024-08-03 19:47:51 +0530

    dsf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "backendrg"
-    storage_account_name = "backendsa107"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
-}
